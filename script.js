09d33ca1e37a2fd733ca9340dfd8a0d8462478e0
ChanduBobbili <84794638+ChanduBobbili@users.noreply.github.com>
2021-05-26 12:37:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute(){
     
     var year = new Date().getFullYear()+parseInt(r);
     
-    document.getElementById('result').innerHTML = "If you deposit "+p+"<br> at an interest rate of "+t+"%"+"<br> you will receive an amount of <b>"+si+"</b> <br> in the year of "+year;
+    document.getElementById("result").innerHTML = "If you deposit "+p+"<br> at an interest rate of "+t+"%"+"<br> you will receive an amount of <b>"+si+"</b> <br> in the year of "+year;
     
     
     
