4c594dc77879199b0dc6c7e30803a75a60efce7c
ChanduBobbili <84794638+ChanduBobbili@users.noreply.github.com>
2021-05-26 19:04:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function compute(){
     var year = new Date().getFullYear()+parseInt(r);
     
     document.getElementById("result").innerHTML = "If you deposit "+p+"<br> at an interest rate of "+t+"%"+"<br> you will receive an amount of <b>"+si+"</b> <br> in the year of "+year;
+    document.getElementById("result").innerHTML = "hi";
     
+}    
     
-    
-    
-}
+
 
 function updateRate(){
     var rateval = document.getElementById("rate").value;
